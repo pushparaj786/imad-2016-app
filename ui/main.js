e92d0248369b11070d2c3a34e55f4874f9e14c63
pushparaj786 <pushparaj786@gmail.com>
2016-10-02 00:09:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,6 +31,6 @@ submit.onclick=function(){
     for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
