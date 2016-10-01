7c7f6fffd0565fb36a29b70853c0a1a72f4d1325
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 13:20:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
+
+//Move the image
+var img=document.getElementById('madi');
+img.onclick=function () {
+img.style.marginLeft='100px';    
+    
+};
