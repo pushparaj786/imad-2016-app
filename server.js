f5e20187c60208c3db63b89ad71dfbe75df288fa
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 12:29:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ app.get('/:articleName', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
