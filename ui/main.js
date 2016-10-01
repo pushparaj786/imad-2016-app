88a6454279c6a70ff6fa254891ea145245a0a7d5
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 21:32:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 //counter code
 var button = document.getElementById('counter'); 
 
-button.onclick=function(){
+button.onclick = function(){
 //create a request object
 
 var request=new XMLHttpRequest();
 
-request.onreadystatechange=function(){
+request.onreadystatechange = function() {
     if(request.readyState===XMLHttpRequest.DONE){
          //Take some action
     if(request.Status===200){
-        var counter=request.responseText;
-        var span=document.getElementById('count');
+        var counter = request.responseText;
+        var span = document.getElementById('count');
         span.innerHTML=counter.toString();
     }
   }
