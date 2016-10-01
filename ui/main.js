b1b71d66ad525b02c95c3b19b1521d04a77cedb0
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 23:54:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,17 @@ request.onreadystatechange = function() {
 request.open('GET','http://pushparaj786.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value
+var submit=document.getElementById('submit-btn');
+submit.onclick=function(){
+    //Make a request to the server and send the name
+    //Capture a list of names and render it as a list
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+-'<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
