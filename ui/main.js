ffd2fcd23e3e44df1fb6fded931b093a503d1f40
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 20:52:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //counter code
 var button = document.getElementById('counter'); 
 
-button.onClick=function(){
+button.onclick=function(){
 //create a request object
 
 var request= new XMLHttpRequest();
