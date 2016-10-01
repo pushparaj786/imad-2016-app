a399a7e114bd4bde28edad1e96c8385be1af4166
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 13:39:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ element.innerHTML='NEW VALUE';
 
 //Move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+  marginLeft=marginLeft+10;
+  img.style.marginLeft=marginLeft+'px';
+ };
+
 img.onclick=function () {
-img.style.marginLeft='100px';    
+var interval=setInterval(marRight,50);   
     
 };
