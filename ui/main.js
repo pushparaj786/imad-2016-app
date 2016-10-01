b2910bf2aef913a28386a1697b0f33569beaabc8
pushparaj786 <pushparaj786@gmail.com>
2016-10-01 13:39:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var marginLeft=0;
 function moveRight(){
   marginLeft=marginLeft+10;
   img.style.marginLeft=marginLeft+'px';
- };
+ }
 
 img.onclick=function () {
 var interval=setInterval(marRight,50);   
