6e0e1d9484bd9a2ace1a8e583b4f4d36bf9be741
pushparaj786 <pushparaj786@gmail.com>
2016-10-02 12:40:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,6 +49,6 @@ submit.onclick=function(){
 //Make the request
 var nameInput=document.getElementById('name');
 var name = nameInput.value;
-request.open('GET','http://pushparaj786.imad.hasura-app.io/submit-name?name' + name,true);
+request.open('GET','http://pushparaj786.imad.hasura-app.io/submit-name?name=' + name,true);
 request.send(null);
 };
