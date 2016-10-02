370ff48c7b2326bf813dcbdeb41c744031ed0965
pushparaj786 <pushparaj786@gmail.com>
2016-10-02 11:25:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res){
     //Get the name from the request
-    var names=req.params.name;
+    var name = req.params.name;
     names.push(name);
     //JSON:Javascript Object Notation
     res.send(JSON.stringify(names));
