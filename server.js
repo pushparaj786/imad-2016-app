ad2c44893cc313ec9f04f8f477538da89d5e6bdd
pushparaj786 <pushparaj786@gmail.com>
2016-10-24 21:03:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 
-var config={
+var config = {
     user:'pushparaj786',
     database:'pushparaj786',
     host:'db.imad.hasura-app.io',
     port:'5432'
     password: process.env.DB_PASSWORD
-    };
+};
+    
+    
 
 
 
