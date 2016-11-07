a66bebd94a60ac18c66138d696a12959febd5916
pushparaj786 <pushparaj786@gmail.com>
2016-11-07 20:54:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var path = require('path');
 var Pool= require('pg').Pool;
 var config = {
     user:'pushparaj786',
-    database:'pushparaj786',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
+   database:'pushparaj786',
+  host:'db.imad.hasura-app.io',
+   port:'5432',
+   password:process.env.DB_PASSWORD
 };
 
 var app = express();
@@ -88,13 +88,13 @@ var pool= new Pool(config);
 app.get('/test-db',function(req.res){
     //make a select request
     //return a response with the results
-    pool.query('SELECT * FROM test',function(err,result){
-      if (err) {
-          res.status(500),send(err,toString());
-        }else {
-            res.send(JSON,stringify(result));
-        }
-    });
+   pool.query('SELECT * FROM test',function(err,result){
+     if (err) {
+         res.status(500),send(err,toString());
+      }else {
+           res.send(JSON,stringify(result));
+       }
+   });
 });
 
 
