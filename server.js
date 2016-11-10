299ad1aa591b6f6c9144bffabbaaf1bfc41a4ec8
pushparaj786 <pushparaj786@gmail.com>
2016-11-10 20:20:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,10 +120,10 @@ app.get('/articles/:articleName', function (req, res) {
    pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + "'",function(err,result){
       if(err) {
           res.status(500).send(err.toString());
-      }else{
-          if (results.rows.length===0){
+      } else {
+          if (result.rows.length===0){
             res.status(404).send('Artilce Not found');  
-          }else{
+          } else {
               var articleData=result.rows[0];
               res.send(createTemplate(articleData));
           }
