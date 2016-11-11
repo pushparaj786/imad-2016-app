69984e0325bceb2d0192e83429f4c53ab730d712
pushparaj786 <pushparaj786@gmail.com>
2016-11-11 20:01:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan=require('morgan');
 var path=require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
-//var bodyParser=require('body-parser');
+var bodyParser=require('body-parser');
 var config = {
     user: 'pushparaj786',
     database: 'pushparaj786',
@@ -14,7 +14,7 @@ var config = {
 
 var app=express();
 app.use(morgan('combined'));
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 function createTemplate(data){
     var title=data.title;
     var date=data.date;
@@ -71,13 +71,13 @@ app.get('/hash/:input',function(req,res){
 });
 
 
-//app.post('/create-user',function(req,res){
+app.post('/create-user',function(req,res){
 //username,password
-//var username = req.body.username;
-//var password = req.body.password;
-//var salt = crypto.getRandomBytes(128).toString('hex');
-//var dbString = hash(password,salt);
-//pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+var username = req.body.username;
+var password = req.body.password;
+var salt = crypto.getRandomBytes(128).toString('hex');
+var dbString = hash(password,salt);
+pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
     if (err){
             res.status(500).send(err.toString());
         } else {
