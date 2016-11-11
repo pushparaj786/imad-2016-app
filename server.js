59af51f596147092fe7ad7a4db6b9aeac0e6fabb
pushparaj786 <pushparaj786@gmail.com>
2016-11-11 16:03:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan=require('morgan');
 var path=require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
-var bodyParser=require('body-parser');
+//var bodyParser=require('body-parser');
 var config = {
     user: 'pushparaj786',
     database: 'pushparaj786',
@@ -14,7 +14,7 @@ var config = {
 
 var app=express();
 app.use(morgan('combined'));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 function createTemplate(data){
     var title=data.title;
     var date=data.date;
