d4ae22ae34aeb3ef768bd877a2cd9256c1192766
pushparaj786 <pushparaj786@gmail.com>
2016-11-18 12:34:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@
 var submit = document.getElementById ('submit_btn');
 submit.onclick = function() {
             //create a request object 
-    var request=new XMLHttpRequest();
+    var request = new XMLHttpRequest();
             //Capture the response and store it in a variable
     request.onreadystatechange = function() {
     if(request.readyState===XMLHttpRequest.DONE){
          //Take some action
-     if(request.status===200){
+     if(request.status === 200){
         alert('Logged in successfully!');
     } else if (request.status === 403)  {
         alert('Username/password is incorrect');
@@ -23,5 +23,6 @@ var password = document.getElementById('password').value;
 console.log('username');
 console.log('password');
 request.open('POST','http://pushparaj786.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content_Type','application/json');
 request.send(JSON.stringify({username: username, password: password}));
 };
