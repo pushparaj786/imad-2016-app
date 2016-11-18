2f89a03be938d7443bd17a6be429cbc66612cbfc
pushparaj786 <pushparaj786@gmail.com>
2016-11-18 12:29:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,54 +1,27 @@
-//counter code
-var button = document.getElementById('counter'); 
 
-button.onclick = function(){
-//create a request object
-
-var request=new XMLHttpRequest();
-
-request.onreadystatechange = function() {
-    if(request.readyState===XMLHttpRequest.DONE){
-         //Take some action
-    if(request.status===200){
-        var counter = request.responseText;
-        var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-    }
-  }
-};
-
-//Make the request
-request.open('GET','http://pushparaj786.imad.hasura-app.io/counter',true);
-request.send(null);
-};
-
-
-//Submit name through input box
-
-var submit = document.getElementById ('submit-btn');
-submit.onclick=function(){
+//Submit username/password to login
+var submit = document.getElementById ('submit_btn');
+submit.onclick = function() {
             //create a request object 
     var request=new XMLHttpRequest();
             //Capture the response and store it in a variable
     request.onreadystatechange = function() {
     if(request.readyState===XMLHttpRequest.DONE){
          //Take some action
-    if(request.status===200){
-             //Capture a list of names and render it as a list
-    var names = request.responseText;
-    names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
+     if(request.status===200){
+        alert('Logged in successfully!');
+    } else if (request.status === 403)  {
+        alert('Username/password is incorrect');
+    } else if(request.status === 500)  {
+        alert('something went wrong on the server');
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
 }
-  }
 };
 //Make the request
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','http://pushparaj786.imad.hasura-app.io/submit-name?name=' + name,true);
-request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log('username');
+console.log('password');
+request.open('POST','http://pushparaj786.imad.hasura-app.io/login', true);
+request.send(JSON.stringify({username: username, password: password}));
 };
