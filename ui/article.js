6ee8eb7a83ca0eb1700cfafc1821748b9e57b9a2
pushparaj786 <pushparaj786@gmail.com>
2016-11-21 16:11:35 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,1 +1,10 @@
-
+//eg: pushparaj786.imad.hasura-app.io/articles/article-one will result in aticlename
+var currentArticleTitle = window.location.pathname.split('/')[2];
+function loadCommentForm(){
+    var commentFormHtml = `
+    <h5>Submit a comment</h5>
+    <textarea id ="comment_text" rows="5" cols = "100" placeholder = "Enter your comment here ....."></textarea>
+    <br/>
+    <input type = "submit" id="submit" value = "Submit"/>
+    <br/>
+    `}
