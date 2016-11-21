c40445ca1dec9054f77ec2259bd15adf6566648c
pushparaj786 <pushparaj786@gmail.com>
2016-11-21 12:42:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-var loginHtml='
-    <h3>Login/Register to unlock awesome features</h3>
+var loginHtml=
+    `<h3>Login/Register to unlock awesome features</h3>
     <input type = "text" id ="username" placeholder ="username"/>
     <input type = "password" id ="password"/>
     <hr/><hr/>
     <input type = "submit" id ="login_btn" value ="Login"/>
     <input type = "submit" id ="register_btn" value ="Register"/>
-    ';
+    `;
     document.getElementById('login_area').innerHTML = loginHtml;
 
 //Submit username/password to login
@@ -71,10 +71,10 @@ register.value = 'Registering....';
 
 function loadLoggedInUser(username){
  var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML='
+    loginArea.innerHTML=`
     <h3>Hi<i>${username}</i></h3>
     <a href = "/logout">Logout</a>
-    ';
+    `;
 }
 
 function loadLogin(){
@@ -93,6 +93,36 @@ function loadLogin(){
  };
     request.open('GET','/check-login',true);
     request.send(null);
+}
+
+function loadArticles(){
+   //check if the user is already logged in
+ var request = new XMLHttpRequest();
+                          //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState===XMLHttpRequest.DONE) {
+         var articles = document.getElementById('articles');
+         if(request.status === 200){
+            var content = '<ul>';
+            var articleData = JSON.parse(this.responseText);
+            for (var i=0; i<articleData.length; i++){
+                content += `<li> <a href = "/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                (${articleData[i].date.split('T')[0]}),</li>`;
+            }
+            content += "</ul>";
+            articles.innerHTML = content;
+            } else  {
+        articles.innerHTML('Oops!Could not load all articles!');
+    }
+   }
+ };
+    request.open('GET','/get-articles',true);
+    request.send(null);
+} 
+  //The first thing to do is to check if the user is logged in!
+  loadLogin();
+  //Now this is something that we could have directly done on the server side using templating too!
+  loadArticles();
 }
 
 
