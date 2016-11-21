491602aba49b6bf368bc9786158aa8e95bf908a0
pushparaj786 <pushparaj786@gmail.com>
2016-11-21 11:32:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
+var loginHtml='
+    <h3>Login/Register to unlock awesome features</h3>
+    <input type = "text" id ="username" placeholder ="username"/>
+    <input type = "password" id ="password"/>
+    <hr/><hr/>
+    <input type = "submit" id ="login_btn" value ="Login"/>
+    <input type = "submit" id ="register_btn" value ="Register"/>
+    ';
+    document.getElementById('login_area').innerHTML = loginHtml;
 
 //Submit username/password to login
 var submit = document.getElementById ('submit_btn');
