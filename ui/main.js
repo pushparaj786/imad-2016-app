77545e7fb9159c278a584a394b09af2e4f644467
pushparaj786 <pushparaj786@gmail.com>
2016-11-21 11:57:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,12 +28,54 @@ var submit = document.getElementById ('login_btn');
       loadLogin();
     }
 };
+
+
 //Make the request
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log('username');
 console.log('password');
-request.open('POST','http://pushparaj786.imad.hasura-app.io/login', true);
+request.open('POST','/login', true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username: username, password: password}));
+submit.value = 'Logged in....';
 };
+
+var register = document.getElementById ('register_btn');
+    register.onclick = function(){
+                           //create a request object 
+    var request = new XMLHttpRequest();
+                          //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState===XMLHttpRequest.DONE) {
+         //Take some action
+         if(request.status === 200){
+             alert('User created successfully');
+             register.value = 'Registered!';
+    } else  {
+        alert('Could not register the user');
+        register.value = 'Register';
+    }
+   }
+ };
+//Make the request
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log('username');
+console.log('password');
+request.open('POST','/create-user', true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username: username, password: password}));
+register.value = 'Registering....';
+};
+
+
+
+
+
+
+
+
+
+
+
