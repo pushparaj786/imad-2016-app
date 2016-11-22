97e362f99d395f4dd1bc83db1f32f7699b83c545
pushparaj786 <pushparaj786@gmail.com>
2016-11-22 15:57:52 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,9 +3,9 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 function loadCommentForm(){
     var commentFormHtml = `
     <h5>Submit a comment</h5>
-    <textarea id ="comment_text" rows="5" cols = "100" placeholder = "Enter your comment here ....."></textarea>
+    <textarea id ="comment_text" rows="5" cols="100" placeholder="Enter your comment here ....."></textarea>
     <br/>
-    <input type = "submit" id="submit" value = "Submit"/>
+    <input type="submit" id="submit" value = "Submit"/>
     <br/>
     `;
 document.getElementById('comment_form').innerHTML = commentFormHtml;
@@ -26,7 +26,7 @@ var submit = document.getElementById('submit');
     } else  {
         alert('Error! Could not submit comment');
     }
-      submit.value='Submit';
+        submit.value='Submit';
     }
 };
 
