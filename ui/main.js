81d14d64dc18ac65141abc53495b7493320f920b
pushparaj786 <pushparaj786@gmail.com>
2016-12-02 06:42:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -128,9 +128,9 @@ function loadArticles(){
     request.send(null);
 } 
   //The first thing to do is to check if the user is logged in!
-  loadLogin();
+  
   //Now this is something that we could have directly done on the server side using templating too!
-  loadArticles();
+  
 
 
 
