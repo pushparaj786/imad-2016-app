723a89de6935c3e274ccb50d03fce4674e86b69d
pushparaj786 <pushparaj786@gmail.com>
2016-12-02 06:29:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,8 @@ var submit = document.getElementById ('login_btn');
      if(request.status === 200){
         submit.value = 'Success!';
     } else if (request.status === 403)  {
-        submit.value = 'Invalid credentials,Try again?';
+        alert('invalid credentials.Try again')
+        submit.value = 'Login';
     } else if(request.status === 500)  {
         alert('something went wrong on the server');
         submit.value = 'Login';
